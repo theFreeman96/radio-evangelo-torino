e964455bb33512899a9f2c8af0be390eff402388
Davide Belvisi <davide.belvisi96gmail.com>
2025-01-28 23:08:52 +0100

    import optimization

--- lib/screens/news_detail_page.dart
+++ lib/screens/news_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:radio_evangelo_torino/utilities/constants.dart';
 
 import '../utilities/constants.dart';
 import '../utilities/theme_provider.dart';
